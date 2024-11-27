28c76b6a7db241bf29e16f9ed84db82e8be88093
xuanhao05 <nguyenxuanhao1812@gmail.com>
2024-11-27 09:10:00 +0700

    cau2_11.py

---
+++ Bai2/cau2_11.py
@@ -0,0 +1,10 @@
+print("sv:Nguyen Xuan Hao")
+print("mssv:235752021610096")
+1=[1, 'python',4,7]
+k=['cse', 2, 'guntur',8]
+m=[]
+m.append(1);
+m.append(k);
+print (m)
+d={1:1,2:k, 'combine list':m}
+print (d)
