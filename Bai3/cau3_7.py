9411ca9142d9b4e16a4bf4b868ac149f4035bdaf
xuanhao05 <nguyenxuanhao1812@gmail.com>
2024-11-27 10:12:34 +0700

    cau3_7.py

---
+++ Bai3/cau3_7.py
@@ -0,0 +1,8 @@
+print("sv:Nguyen Xuan Hao")
+print("mssv:235752021610096")
+def checkValue(n):
+    if n%2 == 0:  
+      print ("Đây là một số chẵn")
+    else:  
+      print ("Đây là một số lẻ")
+checkValue(7) 
