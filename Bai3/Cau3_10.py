630ec6f804a0656578725f80370e2d4c1336c516
xuanhao05 <nguyenxuanhao1812@gmail.com>
2024-11-27 10:24:55 +0700

    Cau3_10.py

---
+++ Bai3/Cau3_10.py
@@ -0,0 +1,10 @@
+print("sv:Nguyen Xuan Hao")
+print("mssv:235752021610096")
+R=float(input("nhap ban kinh r: "))
+import math;
+def Tinh (R):
+    chu vi = 2*math.pi*R
+    print("chu vu hinh tron:", chu vi)
+    Dien tich = math.pi*R**2
+    print(" Dien tich hinh tron: ", dien tich)
+Tinh(R)
