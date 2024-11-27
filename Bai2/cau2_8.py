443b6a0c2f1e04bf5f3628fd5efa14ca8a4da502
xuanhao05 <nguyenxuanhao1812@gmail.com>
2024-11-27 08:58:39 +0700

    cau2_8.py

---
+++ Bai2/cau2_8.py
@@ -0,0 +1,11 @@
+print("sv:Nguyen Xuan Hao")
+print("mssv:235752021610096")
+a, b = 1, 2
+total = 0
+print(a,end=" ")
+while (a <=4000000-1):
+    if a % 2 == 0:
+         total += a
+    a, bb, a+b
+    print(a,end="")
+print("\n sum of prime numbers term in fibonacci series: ",total)
