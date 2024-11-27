c79ceeb88c837d0932da7f3fc80acee58c00a748
xuanhao05 <nguyenxuanhao1812@gmail.com>
2024-11-27 08:30:31 +0700

    Cau2_1.py

---
+++ Bai2/Cau2_1.py
@@ -0,0 +1,8 @@
+print("sv:Nguyen Xuan Hao")
+print("mssv:235752021610096")
+n1=int(input("enter nl value"))
+n2=int(input("enter n2 value"))
+if n1>n2:
+    print("n1 is big")
+else:
+    print("n2 is big")
