1c59d7f5379033a768e484819b2ba4c4e0e320a9
xuanhao05 <nguyenxuanhao1812@gmail.com>
2024-11-29 22:25:39 +0700

    Cau4_18.py

---
+++ Bai4/Cau4_18.py
@@ -0,0 +1,14 @@
+print("sv:Nguyen Xuan Hao")
+print("mssv:235752021610096)
+n = int(input("Nhập số nguyên n: "))
+
+    fibonacci_list = []
+    a, b = 0, 1
+    while a < n:
+        fib_list.append(a)
+        a, b = b, a + b
+
+print("Danh sách các số Fibonacci nhỏ hơn", n, "là:", fib_numbers)
+
+
+
