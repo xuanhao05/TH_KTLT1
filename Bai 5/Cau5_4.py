78d95cf12021dee879392d0de7707c3bf54264eb
xuanhao05 <nguyenxuanhao1812@gmail.com>
2024-11-29 22:57:02 +0700

    Cau5_4.py

---
+++ Bai 5/Cau5_4.py
@@ -0,0 +1,7 @@
+print("sv:Nguyen Xuan Hao")
+print("mssv:235752021610096")
+import numpy as np
+array = np.arange(12, 39)
+reversed_array = array[::-1]
+print("Mảng ban đầu:", array)
+print("Mảng đảo ngược:", reversed_array)
