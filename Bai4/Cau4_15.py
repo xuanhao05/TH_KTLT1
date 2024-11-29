f14024f93f4c3bbce8d3db6b2e9d7b257d356ea7
xuanhao05 <nguyenxuanhao1812@gmail.com>
2024-11-29 22:12:20 +0700

    Cau4_15.py

---
+++ Bai4/Cau4_15.py
@@ -0,0 +1,7 @@
+print("sv:Nguyen Xuan Hao")
+print("mssv:235752021610096)
+ds = input('nhap chuoi: ').split()
+danh_sach = sorted(ds)
+print("cac tu theo tu dien: ")
+for ds in danh_sach:
+    print(ds)
