a915aa4681d639547036c6ea8ac8751afdd5fb49
xuanhao05 <nguyenxuanhao1812@gmail.com>
2024-12-02 09:08:41 +0700

    Cau5_10module.py

---
+++ Bai 5/Cau5_10module.py
@@ -0,0 +1,9 @@
+print("sv:Nguyen Xuan Hao")
+print("mssv:235752021610096")
+def bubblesort (nlist):   
+n=len(nlist)
+for i in range(n)
+   for j in range(0,n - i - 1):
+       if nlist[j] > nlist[j+1]:
+         nlist[j], nlist[j+1]=nlist[j+1], nlist[j]
+return nlist
