b3470ab74ee20dd16465fe0fe46f0d9ad1b2ed8a
xuanhao05 <nguyenxuanhao1812@gmail.com>
2024-12-02 09:15:00 +0700

    cau5_1.py

---
+++ Bai 5/cau5_1.py
@@ -0,0 +1,13 @@
+print("sv:Nguyen Xuan Hao")
+print("mssv:235752021610096")
+## File: mymath.py ## 
+def square(n):     
+    return n*n 
+def cube(n): 
+   return n*n*n 
+def average(values):   
+   nvals = len(values)    
+   sum = 0.0     
+   for v in values:       
+      sum += v      
+return float(sum)/nvals 
