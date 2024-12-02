1f4889ab13390e56bd9034c49d077841a6f6e8b5
xuanhao05 <nguyenxuanhao1812@gmail.com>
2024-12-02 09:16:44 +0700

    cau6_1.py

---
+++ Bai 6/cau6_1.py
@@ -0,0 +1,10 @@
+print("sv:Nguyen Xuan Hao")
+print("mssv:235752021610096")
+class Circle (object):
+    def __init_(self, r):
+      self.radius = r
+    def area (self):
+      return self.radius**2*3.14
+      
+aCircle = Circle(2)
+print (aCircle.area())
