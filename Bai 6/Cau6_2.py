d900ab7bd549daa1a30feda2f0666aa79feae657
xuanhao05 <nguyenxuanhao1812@gmail.com>
2024-12-02 09:17:42 +0700

    Cau6_2.py

---
+++ Bai 6/Cau6_2.py
@@ -0,0 +1,10 @@
+print("sv:Nguyen Xuan Hao")
+print("mssv:235752021610096")
+class Hinhchunhat:
+    def __init__(self, chieu_dai, chieu_rong):
+        self.chieu_dai = chieu_dai
+        self.chieu_rong = chieu_rong
+    def tinh_dien_tich(self):
+                 return self.chieu_dai * self.chieu_rong
+hcn = Hinhchunhat(2, 3)
+print("Diện tích hình chữ nhật:", dien_tich)
